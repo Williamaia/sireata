cec4921a8a163d7c098ee5e61483acd8f7915ecc
williamaiadias <williamaiadias@hotmail.com>
2020-10-01 18:55:13 -0300

    Tentando adicionar Try-With-Resources na classe CampusDAO para evitar que ao ocorrer exceções no metodo close, essas exceções sobrepor as exeções do try

--- src/main/java/br/edu/utfpr/dv/sireata/dao/CampusDAO.java
+++ src/main/java/br/edu/utfpr/dv/sireata/dao/CampusDAO.java
@@ -14,138 +14,97 @@ import br.edu.utfpr.dv.sireata.model.Campus;
 public class CampusDAO {
 	
 	public Campus buscarPorId(int id) throws SQLException{
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
+		String sql ="SELECT * FROM campus WHERE idCampus = ?";
 		
-		try{
-			conn = ConnectionDAO.getInstance().getConnection();
-			stmt = conn.prepareStatement("SELECT * FROM campus WHERE idCampus = ?");
-		
-			stmt.setInt(1, id);
-			
-			rs = stmt.executeQuery();
+		try (
+				Connection conn = ConnectionDAO.getInstance().getConnection();
+				Statement stmt = conn.prepareStatement(sql);
+				ResultSet rs = stmt.executeQuery(sql);
+			){
 			
 			if(rs.next()){
 				return this.carregarObjeto(rs);
 			}else{
 				return null;
 			}
-		}finally{
-			if((rs != null) && !rs.isClosed())
-				rs.close();
-			if((stmt != null) && !stmt.isClosed())
-				stmt.close();
-			if((conn != null) && !conn.isClosed())
-				conn.close();
 		}
 	}
 	
 	public Campus buscarPorDepartamento(int idDepartamento) throws SQLException{
-		Connection conn = null;
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
+		String sql ="SELECT idCampus FROM departamentos WHERE idDepartamento=?";
 		
-		try{
-			conn = ConnectionDAO.getInstance().getConnection();
-			stmt = conn.prepareStatement("SELECT idCampus FROM departamentos WHERE idDepartamento=?");
-		
-			stmt.setInt(1, idDepartamento);
-			
-			rs = stmt.executeQuery();
+		try (
+				Connection conn = ConnectionDAO.getInstance().getConnection();
+				Statement stmt = conn.prepareStatement(sql);
+				ResultSet rs = stmt.executeQuery(sql);
+			) {
 			
 			if(rs.next()){
 				return this.buscarPorId(rs.getInt("idCampus"));
 			}else{
 				return null;
 			}
-		}finally{
-			if((rs != null) && !rs.isClosed())
-				rs.close();
-			if((stmt != null) && !stmt.isClosed())
-				stmt.close();
-			if((conn != null) && !conn.isClosed())
-				conn.close();
 		}
 	}
 	
 	public List<Campus> listarTodos(boolean apenasAtivos) throws SQLException{
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
+		String sql = "SELECT * FROM campus \" + (apenasAtivos ? \" WHERE ativo=1\" : \"\") + \" ORDER BY nome";
 		
-		try{
-			conn = ConnectionDAO.getInstance().getConnection();
-			stmt = conn.createStatement();
-		
-			rs = stmt.executeQuery("SELECT * FROM campus " + (apenasAtivos ? " WHERE ativo=1" : "") + " ORDER BY nome");
-		
-			List<Campus> list = new ArrayList<Campus>();
-			
-			while(rs.next()){
-				list.add(this.carregarObjeto(rs));
+		try (
+				Connection conn = ConnectionDAO.getInstance().getConnection();
+				Statement stmt = conn.createStatement();				
+				ResultSet rs = stmt.executeQuery(sql);
+		    ){
+				List<Campus> list = new ArrayList<Campus>();
+				
+				while(rs.next()) {
+					list.add(this.carregarObjeto(rs));
+				}
+				
+				return list;
+				
 			}
-			
-			return list;
-		}finally{
-			if((rs != null) && !rs.isClosed())
-				rs.close();
-			if((stmt != null) && !stmt.isClosed())
-				stmt.close();
-			if((conn != null) && !conn.isClosed())
-				conn.close();
-		}
 	}
+
 	
 	public List<Campus> listarParaCriacaoAta(int idUsuario) throws SQLException{
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
+		String sql = "SELECT DISTINCT campus.* FROM campus \" +\r\n" + 
+				     "\"INNER JOIN departamentos ON departamentos.idCampus=campus.idCampus \" +\r\n" + 
+				     "\"INNER JOIN orgaos ON orgaos.idDepartamento=departamentos.idDepartamento \" +\r\n" + 
+				     "\"WHERE campus.ativo=1 AND (orgaos.idPresidente=\" + String.valueOf(idUsuario) + \" OR orgaos.idSecretario=\" + String.valueOf(idUsuario) + \r\n" + 
+				     "\") ORDER BY campus.nome";
 		
-		try{
-			conn = ConnectionDAO.getInstance().getConnection();
-			stmt = conn.createStatement();
-		
-			rs = stmt.executeQuery("SELECT DISTINCT campus.* FROM campus " +
-				"INNER JOIN departamentos ON departamentos.idCampus=campus.idCampus " +
-				"INNER JOIN orgaos ON orgaos.idDepartamento=departamentos.idDepartamento " +
-				"WHERE campus.ativo=1 AND (orgaos.idPresidente=" + String.valueOf(idUsuario) + " OR orgaos.idSecretario=" + String.valueOf(idUsuario) + 
-				") ORDER BY campus.nome");
-		
+		try(
+				Connection conn = ConnectionDAO.getInstance().getConnection();
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(sql);
+			) {
+			 
 			List<Campus> list = new ArrayList<Campus>();
 			
 			while(rs.next()){
 				list.add(this.carregarObjeto(rs));
 			}
-			
-			return list;
-		}finally{
-			if((rs != null) && !rs.isClosed())
-				rs.close();
-			if((stmt != null) && !stmt.isClosed())
-				stmt.close();
-			if((conn != null) && !conn.isClosed())
-				conn.close();
-		}
+				return list;
+			}
 	}
+		
 	
 	public List<Campus> listarParaConsultaAtas(int idUsuario) throws SQLException{
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
+		String sql ="SELECT DISTINCT campus.* FROM campus \" +\r\n" + 
+					"\"INNER JOIN departamentos ON departamentos.idCampus=campus.idCampus \" +\r\n" + 
+					"\"INNER JOIN orgaos ON orgaos.idDepartamento=departamentos.idDepartamento \" +\r\n" + 
+					"\"INNER JOIN atas ON atas.idOrgao=orgaos.idOrgao \" +\r\n" + 
+					"\"INNER JOIN ataParticipantes ON ataParticipantes.idAta=atas.idAta \" +\r\n" + 
+					"\"WHERE atas.publicada=0 AND ataParticipantes.presente=1 AND ataParticipantes.idUsuario=\" + String.valueOf(idUsuario) + \r\n" + 
+					"\" ORDER BY campus.nome";
 		
-		try{
-			conn = ConnectionDAO.getInstance().getConnection();
-			stmt = conn.createStatement();
-		
-			rs = stmt.executeQuery("SELECT DISTINCT campus.* FROM campus " +
-				"INNER JOIN departamentos ON departamentos.idCampus=campus.idCampus " +
-				"INNER JOIN orgaos ON orgaos.idDepartamento=departamentos.idDepartamento " +
-				"INNER JOIN atas ON atas.idOrgao=orgaos.idOrgao " +
-				"INNER JOIN ataParticipantes ON ataParticipantes.idAta=atas.idAta " +
-				"WHERE atas.publicada=0 AND ataParticipantes.presente=1 AND ataParticipantes.idUsuario=" + String.valueOf(idUsuario) + 
-				" ORDER BY campus.nome");
-		
+		try (
+				Connection conn = ConnectionDAO.getInstance().getConnection();
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(sql);
+			) {
+			
 			List<Campus> list = new ArrayList<Campus>();
 			
 			while(rs.next()){
@@ -153,15 +112,8 @@ public class CampusDAO {
 			}
 			
 			return list;
-		}finally{
-			if((rs != null) && !rs.isClosed())
-				rs.close();
-			if((stmt != null) && !stmt.isClosed())
-				stmt.close();
-			if((conn != null) && !conn.isClosed())
-				conn.close();
 		}
-	}
+	}	
 	
 	public int salvar(Campus campus) throws SQLException{
 		boolean insert = (campus.getIdCampus() == 0);
