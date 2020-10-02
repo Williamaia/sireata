14409d1eaa623191986ff43623bf8d4766d00601
williamaiadias <williamaiadias@hotmail.com>
2020-10-02 10:40:38 -0300

    Classe de test arrumada

--- src/main/java/br/edu/utfpr/dv/sireata/dao/CampusDAO.java
+++ src/main/java/br/edu/utfpr/dv/sireata/dao/CampusDAO.java
@@ -177,5 +177,4 @@ public class CampusDAO {
 		
 		return campus;
 	}
-
 }
--- src/main/java/br/edu/utfpr/dv/sireata/test/TestCampusDAO.java
+++ src/main/java/br/edu/utfpr/dv/sireata/test/TestCampusDAO.java
@@ -1,11 +1,10 @@
 package br.edu.utfpr.dv.sireata.test;
 
 import static org.junit.Assert.assertEquals;
-import java.util.Optional;
-import javax.ws.rs.client.Client;
-import org.junit.Assert;
-import org.junit.Test;
 
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.runner.RunWith;
 import br.edu.utfpr.dv.sireata.dao.CampusDAO;
 import br.edu.utfpr.dv.sireata.model.Campus;
 
@@ -13,13 +12,35 @@ public class TestCampusDAO {
 	
 	@Test
 	public void testmethodBuscarPorId() {
-		try {
-	        Mockito.when(CampusDAO.findById(any(Integer.class)))
-	                .thenReturn(Optional.of(Client.builder()        
-	                        .id(1)));     
-	    } catch (Exception ex) {
-	        Assert.fail();
-	        
-	    }
+
+			Campus campusDAOTest = new Campus();
+			campusDAOTest.setIdCampus(1);
+			
+			@Mock
+			CampusDAO campusTest = new Mockito.(CampusDAO.class);
+		
+			when(campusTest.buscarPorId(1)).thenReturn(campusTest);    
+			
+			Campus campusResult = campusTest.buscarPorId(1);
+			
+			assertEquals(1, campusResult.getIdCampus());
+			
+	}
+	
+	@Test
+	public void testmethodBuscarDepartamento() {
+
+			Campus campusDAOTest = new Campus();
+			campusDAOTest.setIdCampus(1);
+			
+			@Mock
+			CampusDAO campusTest = new Mock.(CampusDAO.class);
+		
+			when(campusTest.buscarPorDepartamento(1)).thenReturn(campusTest);    
+			
+			Campus campusResult = campusTest.buscarPorDepartamento(1);
+			
+			assertEquals(1, campusResult.getIdCampus());
+			
 	}
 }
